604688c2fa49bb43d4f24cf14b96eaa3091248d9
bcorrea1006 <benitoscorrea@gmail.com>
2025-10-21 09:06:12 -0700

    Setup tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,9 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-        <p>
+        <h1 className="text-3xl font-bold underline">
           Today I'm feeling somewhat saucy. Hmm yes.
-        </p>
+        </h1>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
