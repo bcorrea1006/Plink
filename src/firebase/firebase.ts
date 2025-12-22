3f02695d15c330d36f69b9fb0c6b336547892d5c
bcorrea1006 <benitoscorrea@gmail.com>
2025-12-21 19:33:40 -0800

    imported firebase configuration and updated envrionment variables

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,17 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage"
+import { getAuth } from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDgWOeS5Ac6xx6Ixv0rt4E0w3y33ev0CNs",
-  authDomain: "plink-24eae.firebaseapp.com",
-  projectId: "plink-24eae",
-  storageBucket: "plink-24eae.firebasestorage.app",
-  messagingSenderId: "823501013266",
-  appId: "1:823501013266:web:35d583997699ec07a8f3cb"
+  apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const sotrage = getStorage(app);
