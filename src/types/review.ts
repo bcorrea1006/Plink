72c5754f097762cdf8e4695e725056d96f9470dd
bcorrea1006 <benitoscorrea@gmail.com>
2026-07-29 21:46:39 -0700

    update property ordering to match intended form ordering

--- src/types/review.ts
+++ src/types/review.ts
@@ -1,8 +1,8 @@
 export type Review = {
   id: string;
-  images?: string[];
   rating: number;
   tuning: number;
   access: 'public'| 'private' | 'restricted';
   notes: string;
+  images?: string[];
 }
