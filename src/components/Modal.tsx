4ef195df2dec4e24d30d3ac329ef34079041c3ae
bcorrea1006 <benitoscorrea@gmail.com>
2026-08-15 07:14:38 -0700

    update sizing and placement of CancelButton

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -16,9 +16,9 @@ export function Modal({
 
   return (
     <div className="fixed inset-0 z-50 flex px-3 justify-center items-center bg-black/50 backdrop-blur-sm">
-      <div className={`pointer-events-auto absolute max-w-lg mx-4 h-3/4 z-10002 text-black
+      <div className={`pointer-events-auto absolute max-w-lg w-3/4 mx-4 h-3/4 z-10002 text-black
         bg-white rounded-2xl shadow-lg flex max-h-[80vh] flex-col justify-between items-center`}>
-        <CancelButton onCancel={onToggleModal} className='absolute top-6 left-6'/>
+        <CancelButton onCancel={onToggleModal} className='absolute top-6 right-6 w-1/9'/>
         <PianoForm
           location={location}
           onUpdate={updatePianos}
