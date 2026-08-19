ad372f92dcac81dfdb28376ed72d2a1cf41c02b3
bcorrea1006 <benitoscorrea@gmail.com>
2026-08-18 21:04:53 -0700

    add dark mode support for Modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,7 @@
-import PianoForm from './PianoForm';
+import { useContext } from 'react';
+import { PianoForm } from './PianoForm';
 import { CancelButton } from './CancelButton';
+import { ThemeContext } from './context/ThemeContext';
 import type { PianoDetail } from '../types/piano';
 
 interface ModalProps {
@@ -14,10 +16,16 @@ export function Modal({
   updatePianos
 }: ModalProps ) {
 
+  const { isLight } = useContext(ThemeContext);
+
   return (
-    <div className="fixed inset-0 z-50 flex px-3 justify-center items-center bg-black/50 backdrop-blur-sm">
-      <div className={`pointer-events-auto absolute max-w-lg w-3/4 mx-4 h-3/4 z-10002 text-black
-        bg-white rounded-2xl shadow-lg flex max-h-[80vh] flex-col justify-between items-center`}>
+    <div className={`fixed inset-0 z-50 flex px-3 justify-center items-center
+      bg-black/50 backdrop-blur-sm`}>
+      <div
+        className={`pointer-events-auto absolute max-w-lg w-3/4 mx-4 h-3/4 z-10002 text-black
+          rounded-2xl shadow-lg flex max-h-[80vh] flex-col justify-between items-center
+          ${ isLight ? 'bg-white text-black' : 'bg-[#2c2c2c] text-white'}
+        `}>
         <CancelButton onCancel={onToggleModal} className='absolute top-6 right-6 w-1/9'/>
         <PianoForm
           location={location}
