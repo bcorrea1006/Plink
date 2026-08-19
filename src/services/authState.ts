4c52e867059ec90e0b7f7cd7a45ca40c268e19cc
bcorrea1006 <benitoscorrea@gmail.com>
2026-08-19 13:42:16 -0700

    remove old Firebase leftovers

--- src/services/authState.ts
+++
@@ -1,8 +0,0 @@
-// Optional user State observer helpers
-import { auth } from "../firebase/firebase";
-import { onAuthStateChanged} from "firebase/auth";
-import type { User } from "firebase/auth";
-
-export const subscribeToAuth = (callback: (user: User | null) => void) => {
-  return onAuthStateChanged(auth, callback);
-};
