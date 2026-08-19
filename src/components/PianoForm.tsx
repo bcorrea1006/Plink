5d0ff54e78efa5ea4e29a6e0d3b420ed06b601bd
bcorrea1006 <benitoscorrea@gmail.com>
2026-08-18 21:10:01 -0700

    add dark mode support for Image upload button

--- src/components/PianoForm.tsx
+++ src/components/PianoForm.tsx
@@ -216,7 +216,8 @@ export function PianoForm({
 
         {/* Upload button */}
         <button
-          className={`mx-auto flex px-5 gap-2 items-center bg-gray-300 font-medium py-1 rounded border-dashed border-2 border-gray-400`}
+          className={`mx-auto flex px-5 gap-2 items-center font-medium py-1
+            rounded border-dashed border-2 border-gray-400 bg-black/25`}
           onClick={handleButtonClick}
           >
           <ImagePlus />
@@ -235,7 +236,8 @@ export function PianoForm({
 
       <button
         type='submit'
-        className={`block mx-auto max-w-sm w-3/4 mt-8 bg-blue-500 text-white font-medium py-2 rounded hover:bg-blue-600 transition`}
+        className={`block mx-auto max-w-sm w-3/4 mt-8 bg-blue-500 text-white
+          font-medium py-2 rounded hover:bg-blue-600 transition border`}
       >
         Submit
       </button>
